08e7ba1e7cd577fa6683fe62b982b0d1b7759f70
Akinsaya Delamarre <adelamarre@remote-learner.net>
2012-02-03 15:29:54 -0500

    bumped module version

--- version.php
+++ version.php
@@ -7,5 +7,5 @@
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-$module->version  = 2012013000;  // The current module version (Date: YYYYMMDDXX)
+$module->version  = 2012020200;  // The current module version (Date: YYYYMMDDXX)
 $module->cron     = 0;           // Period for cron to check this module (secs)
